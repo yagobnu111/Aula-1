b2060bcfad0bd6d207222dbe6c206212fb3816b5
900091 <900091@BLUE05>
2019-09-12 17:07:31 -0300

    Aula 4

--- Primos/Primos/Program.cs
+++ Primos/Primos/Program.cs
@@ -10,8 +10,8 @@ namespace Primos
     {
         static void Main(string[] args)
         {
-            /*
-            int n, i, m;
+
+            /*int n, i, m;
             int flag;
             List<int> listaPrimos = new List<int> { };
 
@@ -37,36 +37,48 @@ namespace Primos
                 {
                     Console.WriteLine($"{i} é primo ");
                 }
-            }*/
+            }
 
-            primosLista();
-            Console.ReadKey(); 
-        }
-        static void primosLista()
-        {
-            int n, i, m;
-            List<int> listaPrimos = new List<int> { };
-
-            for (i = 2; i <= 10; i++)
-            {
-                m = i / 2;
-
-                for (n = 2; n <= m; n++)
-                {
-
-                    if (i % n == 0)
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        listaPrimos.Add(i);
-                    }
-
-                }
-            }    
-            Console.WriteLine(listaPrimos[0]);
+            Console.ReadKey();*/
+            PrimosWhile();
             Console.ReadKey();
+        }
+
+        static void PrimosWhile()
+        {
+            var n = 0;
+            var divisor = 0;
+            List<string> listaPrimos = new List<string> { };
+
+            Console.WriteLine("Até que número checar por números primos ? ");
+            n = (Convert.ToInt32(Console.ReadLine()));
+
+            for (int i = 3; i <= n; i++)
+            {
+                divisor = i / 2;
+                var naoPrimo = false;
+
+                for (int x = 3; x <= divisor; x++)
+                {
+                    if (i % x == 0)
+                    {
+                        naoPrimo = true;                  
+                    }
+
+                }   
+                if (naoPrimo == false) 
+                {
+                    listaPrimos.Add(i.ToString());
+                }
+
+            }
+
+            Console.WriteLine($"Esses são os números primos de 0 a {n}");
+
+            for (int y = 1; y < (listaPrimos.Count +1); y++)
+            {
+                Console.WriteLine($"{y}° {listaPrimos[y-1]}");
+            }
         }
     }
 }
